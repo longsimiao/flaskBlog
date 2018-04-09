898cc58da69524dc06438ad64cb5971277374ef7
469611602@qq.com <469611602@qq.com>
2018-04-09 17:54:49 +0800

    before user login

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 # @Time: 3/11/18 5:14 PM
 # @Author: Ferras
 
-from app import db, lm
+from app import db
 
 
 class User(db.Model):
@@ -11,31 +11,8 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-    @property
-    def is_authenticated(self):
-        return True
-
-    @property
-    def is_active(self):
-        return True
-
-    @property
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2
-        except NameError:
-            return str(self.id)  # python 3
-
     def __repr__(self):
         return '<User %r>' % self.nickname
-
-
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -45,4 +22,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post %r>' % (self.body)
+        return '<Post %r>' % self.body
